b289c9e9104ad19fc72250c188d9aa2d0d174c60
Roman <145419317+RomanRossonQUT@users.noreply.github.com>
2023-10-17 20:13:30 +1000

    Added detailed comments to views.py

--- a3_group43/soundscape/views.py
+++ a3_group43/soundscape/views.py
@@ -1,31 +1,35 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from .models import Event
-from . import db
-from datetime import datetime
+from flask import Blueprint, render_template, request, redirect, url_for  # Importing necessary modules
+from .models import Event  # Importing the Event model
+from . import db  # Importing the database
+from datetime import datetime  # Importing datetime module
 
+# Creating a Blueprint for main routes
 mainbp = Blueprint('main', __name__)
 
+# Route for the main page
 @mainbp.route('/')
 def index():
     try:
-        events = db.session.query(Event).all()
+        events = db.session.query(Event).all()  # Querying all events from the database
     except Exception as e:
-        print(f"Error fetching events: {e}")
-        events = []
+        print(f"Error fetching events: {e}")  # Printing the error if there's an issue fetching events
+        events = []  # Setting events to an empty list if there's an error
 
-    return render_template('index.html', events=events)
+    return render_template('index.html', events=events)  # Rendering the main page template with the events
 
+# Route for searching events
 @mainbp.route('/search')
 def search():
-    if 'search' in request.args and request.args['search'] != "":
-        print(request.args['search'])
-        query = "%" + request.args['search'] + "%"
+    if 'search' in request.args and request.args['search'] != "":  # Checking if there is a search query
+        print(request.args['search'])  # Printing the search query
+        query = "%" + request.args['search'] + "%"  # Constructing a query string for the search
         try:
-            events = db.session.query(Event).filter(Event.description.like(query)).all()
+            events = db.session.query(Event).filter(Event.description.like(query)).all()  # Searching events based on the description
         except Exception as e:
-            print(f"Error searching events: {e}")
-            events = []
-        
-        return render_template('index.html', events=events)
+            print(f"Error searching events: {e}")  # Printing the error if there's an issue searching events
+            events = []  # Setting events to an empty list if there's an error
+
+        return render_template('index.html', events=events)  # Rendering the main page template with the search results
     else:
-        return redirect(url_for('main.index'))
+        return redirect(url_for('main.index'))  # Redirecting to the main page if no search query is provided
+
