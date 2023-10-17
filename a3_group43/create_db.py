8d639c55b031efe4f96ed1e279f65d87951d5942
Roman <145419317+RomanRossonQUT@users.noreply.github.com>
2023-10-17 20:14:40 +1000

    Added comments to create_db.py (probably not neccessary but oh well)

--- a3_group43/create_db.py
+++ a3_group43/create_db.py
@@ -1,6 +1,9 @@
-from soundscape import db, create_app
-app = create_app()
-ctx = app.app_context()
-ctx.push()
-db.create_all()
+from soundscape import db, create_app  # Import the necessary modules
+
+app = create_app()  # Create the Flask application
+ctx = app.app_context()  # Create a context for the application
+ctx.push()  # Push the application context
+
+db.create_all()  # Create the database tables using SQLAlchemy's create_all() method
+
 quit()
