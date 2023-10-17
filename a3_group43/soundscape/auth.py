09592115dc38f1c9397dd879b9c3ae088932ebfd
Roman <145419317+RomanRossonQUT@users.noreply.github.com>
2023-10-17 20:11:16 +1000

    Added comments to auth.py so that its much more understandable

--- a3_group43/soundscape/auth.py
+++ a3_group43/soundscape/auth.py
@@ -1,52 +1,56 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
-from .forms import LoginForm, RegisterForm
-from flask_login import login_user, login_required, logout_user
-from flask_bcrypt import generate_password_hash, check_password_hash
-from .models import User
-from . import db
+from .forms import LoginForm, RegisterForm  # Importing necessary forms
+from flask_login import login_user, login_required, logout_user  # Importing login-related functions
+from flask_bcrypt import generate_password_hash, check_password_hash  # Importing password hashing functions
+from .models import User  # Importing the User model
+from . import db  # Importing the database
 
-authbp = Blueprint('auth', __name__ )
+# Creating a Blueprint for authentication routes
+authbp = Blueprint('auth', __name__)
 
+# Route for user registration
 @authbp.route('/register', methods=['GET', 'POST'])
 def register():
-    register = RegisterForm()
-    if (register.validate_on_submit()==True):
-            uname = register.username.data
-            pwd = register.password.data
-            email = register.email.data
-            user = db.session.scalar(db.select(User).where(User.name==uname))
-            if user:
-                flash('Username already exists, please try another')
-                return redirect(url_for('auth.register'))
-            pwd_hash = generate_password_hash(pwd)
-            new_user = User(name=uname, password=pwd_hash, email=email)
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect(url_for('main.index'))
+    register = RegisterForm()  # Creating an instance of the RegisterForm
+    if register.validate_on_submit():  # Checking if the form is validated
+        uname = register.username.data  # Extracting username from the form
+        pwd = register.password.data  # Extracting password from the form
+        email = register.email.data  # Extracting email from the form
+        user = db.session.scalar(db.select(User).where(User.name == uname))  # Querying the database for existing user
+        if user:
+            flash('Username already exists, please try another')  # Flashing a message if the username already exists
+            return redirect(url_for('auth.register'))  # Redirecting to the registration page
+        pwd_hash = generate_password_hash(pwd)  # Generating a hash of the password
+        new_user = User(name=uname, password=pwd_hash, email=email)  # Creating a new User object
+        db.session.add(new_user)  # Adding the new user to the session
+        db.session.commit()  # Committing the changes to the database
+        return redirect(url_for('main.index'))  # Redirecting to the main page after successful registration
     else:
-        return render_template('register.html', form=register, heading='Register')
+        return render_template('register.html', form=register, heading='Register')  # Rendering the registration template
 
+# Route for user login
 @authbp.route('/login', methods=['GET', 'POST'])
 def login():
-    login_form = LoginForm()
-    error = None
-    if(login_form.validate_on_submit()==True):
-        username = login_form.username.data
-        password = login_form.password.data
-        user = db.session.scalar(db.select(User).where(User.username==username))
+    login_form = LoginForm()  # Creating an instance of the LoginForm
+    error = None  # Initializing an error variable
+    if login_form.validate_on_submit():  # Checking if the form is validated
+        username = login_form.username.data  # Extracting username from the form
+        password = login_form.password.data  # Extracting password from the form
+        user = db.session.scalar(db.select(User).where(User.username == username))  # Querying the database for the user
         if user is None:
-            error = 'Incorrect username'
+            error = 'Incorrect username'  # Setting an error message for an incorrect username
         elif not check_password_hash(user.password, password):
-            error = 'Incorrect password'
+            error = 'Incorrect password'  # Setting an error message for an incorrect password
         if error is None:
-            login_user(user)
-            return redirect(url_for('main.index'))
+            login_user(user)  # Logging in the user
+            return redirect(url_for('main.index'))  # Redirecting to the main page after successful login
         else:
-            flash(error)
-    return render_template('login.html', form=login_form, heading='Login')
+            flash(error)  # Flashing the error message
+    return render_template('login.html', form=login_form, heading='Login')  # Rendering the login template
 
+# Route for user logout
 @authbp.route('/logout')
-@login_required
+@login_required  # Requiring the user to be logged in to access this route
 def logout():
-    logout_user()
-    return redirect(url_for('main.index'))
+    logout_user()  # Logging out the user
+    return redirect(url_for('main.index'))  # Redirecting to the main page after logout
